453d6af83f64c6dd3111fe3cc4558e2f28f634a0
znets <kulesha.medias@gmail.com>
2022-12-16 20:12:57 +0200

    modified read

--- src/main/java/com/softserve/itacademy/controller/ReadTaskServlet.java
+++ src/main/java/com/softserve/itacademy/controller/ReadTaskServlet.java
@@ -3,6 +3,7 @@ package com.softserve.itacademy.controller;
 import com.softserve.itacademy.model.Error;
 import com.softserve.itacademy.model.Task;
 import com.softserve.itacademy.repository.TaskRepository;
+import org.springframework.http.HttpStatus;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @WebServlet(value = {"/read-task"})
 public class ReadTaskServlet extends HttpServlet {
@@ -22,19 +25,16 @@ public class ReadTaskServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Task task = TaskRepository.getTaskRepository().read(Integer.parseInt(request.getParameter("id")));
+        Task task = taskRepository.read(Integer.parseInt(request.getParameter("id")));
 
         if (task != null) {
             request.setAttribute("task", task);
             request.getRequestDispatcher("/WEB-INF/pages/read-task.jsp").forward(request, response);
         }else {
+            response.setStatus(404);
             request.setAttribute("error", new Error("Task with ID '" + request.getParameter("id") +
                     "' not found in To-Do List!", "/read-task"));
             request.getRequestDispatcher("/WEB-INF/pages/error.jsp").forward(request, response);
         }
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-
     }
 }
